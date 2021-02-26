bfc64be18b50fe469ad3865260b56bd1cabb35be
Paul Duvall <paul.duvall@myfuturechoice.com>
2021-02-26 07:44:24 +0000

    feat: add general styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,45 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      themeRed: {
+        default: '#f46e6d'
+      },
+      themeGreen: {
+        default: '#58afb9'
+      },
+      themeBlue: {
+        default: '#5989bf'
+      },
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      red: colors.red,
+      yellow: colors.amber,
+      green: colors.emerald,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.violet,
+      pink: colors.pink,
+    },
     extend: {
       backgroundImage: theme => ({
         'banner-image': "url('./images/flowers.jpg')"
       })
     },
+    fontFamily: {
+      title: ['Amatic SC']
+    },
+    inset: {
+      '50': '50px',
+      '100': '100px',
+      '150': '150px'
+    }
   },
   variants: {
     extend: {},
